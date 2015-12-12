0e0b33d1eb20f762a5674a1ad960e19a3c31a9dd
Zoltan Polgar <pozo@gmx.com>
2015-12-12 01:55:51 +0100

    proxy server and a static server

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 var http = require('http'),
-    httpProxy = require('http-proxy');
-
+    httpProxy = require('http-proxy'),
+    static = require('node-static');
 //
 // Create a proxy server with custom application logic
 //
-var proxy = httpProxy.createProxyServer({changeOrigin : true});
-
-// To modify the proxy connection before data is sent, you can listen
-// for the 'proxyReq' event. When the event is fired, you will receive
-// the following arguments:
-// (http.ClientRequest proxyReq, http.IncomingMessage req,
-//  http.ServerResponse res, Object options). This mechanism is useful when
-// you need to modify the proxy request before the proxy connection
-// is made to the target.
-//
-proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  proxyReq.setHeader('Accept-Encoding', 'gzip, deflate, sdch');
-  proxyReq.setHeader('user-agent', 'Mozilla/5.0(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36');
-  proxyReq.setHeader('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8');
+var proxy = httpProxy.createProxyServer(
+    {changeOrigin: true}
+);
+proxy.on('proxyReq', function (proxyReq, req, res, options) {
+    proxyReq.setHeader('Accept-Encoding', 'gzip, deflate, sdch');
+    proxyReq.setHeader('user-agent', 'Mozilla/5.0(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36');
+    proxyReq.setHeader('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8');
 });
-
-var server = http.createServer(function(req, res) {
-  // You can define here your custom logic to handle the request
-  // and then proxy the request.
-  proxy.web(req, res, {
-    target: 'http://api.meetup.com'
-  });
+var server = http.createServer(function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    proxy.web(req, res, {
+        target: 'http://api.meetup.com'
+    });
 });
-console.log("listening on port 8000")
+console.log("listening on port 8000");
 server.listen(8000);
+
+var file = new static.Server('./www');
+
+require('http').createServer(function (request, response) {
+
+    console.log(request.url);
+    console.log(request.headers.accept);
+
+    if('/logo.png' == request.url) {
+        request.addListener('end', function () {
+            file.serveFile('meetup.png', 200, {}, request, response);
+        }).resume();
+    } else {
+        request.addListener('end', function () {
+            file.serveFile('index.html', 200, {}, request, response);
+        }).resume();
+    }
+}).listen(8080);
