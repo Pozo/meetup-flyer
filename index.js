b72b012e2c27e42126dc301f5e93856dbaca7f4f
unknown <Zoltan_Polgar@EPHUBUDW0217.budapest.epam.com>
2015-12-12 17:38:45 +0100

    UI adjustments

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ proxy.on('proxyReq', function (proxyReq, req, res, options) {
     proxyReq.setHeader('Accept-Encoding', 'gzip, deflate, sdch');
     proxyReq.setHeader('user-agent', 'Mozilla/5.0(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36');
     proxyReq.setHeader('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8');
+	//proxyReq.setHeader('Referer', req.headers.referer + '?sign=true&key=3c697639401620276161393434366f3f');
 });
 var server = http.createServer(function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
@@ -18,23 +19,23 @@ var server = http.createServer(function (req, res) {
         target: 'http://api.meetup.com'
     });
 });
-console.log("listening on port 8000");
+console.log("Proxy server listening on port 8000");
 server.listen(8000);
 
 var file = new static.Server('./www');
 
+function stringStartsWith(string, prefix) {
+    return string.slice(0, prefix.length) == prefix;
+}
 require('http').createServer(function (request, response) {
-
-    console.log(request.url);
-    console.log(request.headers.accept);
-
     if('/logo.png' == request.url) {
         request.addListener('end', function () {
             file.serveFile('meetup.png', 200, {}, request, response);
         }).resume();
-    } else {
+	} else {
         request.addListener('end', function () {
             file.serveFile('index.html', 200, {}, request, response);
         }).resume();
-    }
+	}
 }).listen(8080);
+console.log("Http server listening on port 8080");
